402c9a492453bda8baf95fb026fd819b75300b77
Luana Saraiva <luana_saraiva2@hotmail.com>
2022-08-08 23:09:55 -0300

    refactor: splits the transaction list into its own file

---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: transactions.map((transaction) {
+        return Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 10
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple.shade700,
+                    width: 2,
+                  )
+                ),
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${transaction.value.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple.shade700,
+                  )
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    transaction.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    DateFormat('d MMM y').format(transaction.date),
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )
+        );
+      }).toList(),
+    );
+  }
+
+}
