f113ce4f1603a596dc95b3bae0aa384e2eba649a
Luana Saraiva <luana_saraiva2@hotmail.com>
2022-08-08 23:08:46 -0300

    refactor: dividing main.dart into components

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'components/transaction_user.dart';
+
 
 main() => runApp(ExpensesApp());
 
@@ -18,20 +18,6 @@ class ExpensesApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'tênis de corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'conta de luz',
-      value: 210.30,
-      date: DateTime.now(),
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -49,103 +35,9 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             )
           ),
-          Column(
-            children: _transactions.map((transaction) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple.shade700,
-                          width: 2,
-                        )
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        'R\$ ${transaction.value.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple.shade700,
-                        )
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          transaction.title,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
-                        Text(
-                          DateFormat('d MMM y').format(transaction.date),
-                          style: TextStyle(
-                            color: Colors.grey[700],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-              );
-            }).toList(),
-          ),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: <Widget>[
-                  CustomTextField(
-                      label: 'Título'
-                  ),
-                  CustomTextField(
-                      label: 'Valor (R\$)'
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      ElevatedButton(
-                          onPressed: () {},
-                          child: Text('Nova Transação'),
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.purple.shade700
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          )
+          TransactionUser(),
         ],
       )
-    );
-  }
-}
-
-class CustomTextField extends StatelessWidget{
-  final String label;
-
-  const CustomTextField({
-    Key? key,
-    required this.label
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
- return TextField(
-     decoration: InputDecoration(
-       labelText: label
-     ),
     );
   }
 }
