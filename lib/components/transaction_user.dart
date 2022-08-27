1bb4b6ec164a8c53e8082fc9638daa58be7ad827
Luana Saraiva <luana_saraiva2@hotmail.com>
2022-08-27 12:47:02 -0300

    refactor: adds new transactions

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -22,7 +22,49 @@ class _TransactionUserState extends State<TransactionUser> {
     Transaction(
       id: 't2',
       title: 'Conta de Luz',
-      value: 211.30,
+      value: 368.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Conta de Agua',
+      value: 445.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Conta de Telefone',
+      value: 638.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't5',
+      title: 'Conta de Gás',
+      value: 578.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't7',
+      title: 'Roupas',
+      value: 150.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't8',
+      title: 'Alexa',
+      value: 100.80,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't9',
+      title: 'Aluguel',
+      value: 357.90,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't10',
+      title: 'Notebook',
+      value: 3000.30,
       date: DateTime.now(),
     ),
   ];
@@ -44,8 +86,8 @@ class _TransactionUserState extends State<TransactionUser> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        TransactionList(_transactions),
         TransactionForm(_addTransaction),
+        TransactionList(_transactions),
       ],
     );
   }
